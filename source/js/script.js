713b6ee126934fedff610538b4cd48d5b32a136b
Evgeniy Bondar <evgbondar18@gmail.com>
2018-02-21 00:43:12 +0300

    Исправил ошибки npm test

--- source/js/script.js
+++
@@ -1,8 +0,0 @@
-  var menuButton = document.querySelector(".main-nav__toggle-open");
-  var closeButton = document.querySelector(".main-nav__toggle-close");
-  var menu = document.querySelector(".header-nav");
-  menuButton.addEventListener("click", function () {
-    menu.classList.add("modal-content-show");
-    closeButton.classList.add("modal-content-show");
-    menuButton.classList.add("modal-content-close");
-  });
